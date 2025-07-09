7fe0f5b8816d8318609c069cff96f335eaf8e0b0
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-09 17:23:28 +0200

    feat: else

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ const image = document.querySelector('.card-img-top')
 
 // creo l'evento
 button.addEventListener('click' , function()
-){
+{
 // logica
 if(button.innerText == `Accendi`){
   button.innerText = `Spegni`
   image.src = "./assets/img/yellow_lamp.png";
 
+} else {
+  button.innerText = `Accendi`
+  image.src = "./assets/img/white_lamp.png";
 }
 
 
@@ -19,5 +22,5 @@ if(button.innerText == `Accendi`){
 
 }
 
-
+)
 
