aaf2d6426e9a3abe4329ed10bbbfed916d2faf77
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-09 17:31:25 +0200

    final results

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-// LIVE CODING: cliccando sul pulsante, cambio il colore del testo presente nell'elemento con id title-change-color
-// definizione delle variabili e recupero degli elementi del dom che mi servono
 const button = document.querySelector('.btn.btn-success');
 const image = document.querySelector('.card-img-top')
 
