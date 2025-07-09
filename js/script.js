a5b3fa7423cc8a831a3af14beb316a89ce1191e3
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-09 17:20:47 +0200

    feat: addEventListener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log(`hola hola coca cola`)
+
+// LIVE CODING: cliccando sul pulsante, cambio il colore del testo presente nell'elemento con id title-change-color
+// definizione delle variabili e recupero degli elementi del dom che mi servono
+const button = document.querySelector('.btn.btn-success');
+const image = document.querySelector('.card-img-top')
+
+// creo l'evento
+button.addEventListener('click' , function()){}
+
+
+
+
+
+
+
+
